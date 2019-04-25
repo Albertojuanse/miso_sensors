a951a43de0b6aea8e778c545e721cc5cf065162e
Albertojuanse <alberto.sebastian@uam.es>
2019-04-25 15:54:01 +0200

    Initial Commit

---
+++ Sensors test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sensors test
+//
+//  Created by MISO on 25/4/19.
+//  Copyright © 2019 MISO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sensors test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sensors test
+//
+//  Created by MISO on 25/4/19.
+//  Copyright © 2019 MISO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
