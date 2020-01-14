72fed6ebfd4466b8bf0cf9f788b32ed60e8b2775
Albertojuanse <albertojuansebastian@gmail.com>
2020-01-14 16:24:25 +0100

    Monitoring stub

--- Sensors test/ViewControlling/ViewControllerMonitoring.h
+++ Sensors test/ViewControlling/ViewControllerMonitoring.h
@@ -2,12 +2,51 @@
 //  ViewControllerMonitoring.h
 //  Sensors test
 //
-//  Created by MISO on 18/12/19.
+//  Created by Alberto J. on 18/12/19.
 //  Copyright © 2019 MISO. All rights reserved.
 //
 
-#ifndef ViewControllerMonitoring_h
-#define ViewControllerMonitoring_h
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "ViewControllerMainMenu.h"
+#import "Canvas.h"
+
+/*!
+ @class ViewControllerThetaThetaLocating
+ @discussion This class extends UIViewController and controls the interface for locating the device with the theta theta location system.
+ */
+@interface ViewControllerMonitoring : UIViewController <UITableViewDelegate, UITableViewDataSource> {
+    
+    // Other components
+    SharedData * sharedData;
+    MotionManager * motion;
+    LocationManagerDelegate * location;
+    
+    // Session and user context
+    // The first credentials dictionary is for security issues and its proprietary is the one who logs-in in the device; the second one is used for identifying purposes; in multiuser context, the first one is used in the device for accessing data, etc. while the second one is shared to the rest of users when a measure is taken or something is changed to indicate who did it.
+    NSMutableDictionary * credentialsUserDic;
+    NSMutableDictionary * userDic;
+    
+    // Beacons' region identifiers
+    NSNumber * itemBeaconIdNumber;
+    NSNumber * itemPositionIdNumber;
+    NSString * locatedPositionUUID; // This one changes when the user measures and generates a new position for the device.
+    
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *tableRegister;
+@property (weak, nonatomic) IBOutlet Canvas *canvas;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFinish;
+
+- (void) setCredentialsUserDic:(NSMutableDictionary *)givenCredentialsUserDic;
+- (void) setUserDic:(NSMutableDictionary *)givenUserDic;
+- (void) setSharedData:(SharedData *)givenSharedData;
+- (void) setMotionManager:(MotionManager *)givenMotion;
+- (void) setLocationManager:(LocationManagerDelegate *)givenLocation;
+
+- (void) setItemBeaconIdNumber:(NSNumber *)givenRegionIdNumber;
+- (void) setItemPositionIdNumber:(NSNumber *)givenRegionIdNumber;
+
+@end
 
 
-#endif /* ViewControllerMonitoring_h */
--- Sensors test/ViewControlling/ViewControllerMonitoring.m
+++ Sensors test/ViewControlling/ViewControllerMonitoring.m
@@ -1,9 +1,272 @@
 //
-//  ViewControllerMonitoring.m
+//  ViewControllerMonitoring.h
 //  Sensors test
 //
-//  Created by MISO on 18/12/19.
+//  Created by Alberto J. on 18/12/19.
 //  Copyright © 2019 MISO. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "ViewControllerMonitoring.h"
+
+@implementation ViewControllerMonitoring
+
+#pragma mark - UIViewController delegated methods
+
+/*!
+ @method viewDidLoad
+ @discussion This method initializes some properties once the object has been loaded.
+ */
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Register the current mode
+    if (
+        [sharedData validateCredentialsUserDic:credentialsUserDic]
+        )
+    {
+        [sharedData inSessionDataSetMode:@"MONITORING"
+                       toUserWithUserDic:userDic
+                   andCredentialsUserDic:credentialsUserDic];
+    } else {
+        // TO DO: handle intrusion situations. Alberto J. 2019/09/10.
+    }
+    
+    // Initial state
+    [sharedData inSessionDataSetIdleUserWithUserDic:userDic
+                          andWithCredentialsUserDic:credentialsUserDic];
+    
+    // Variables
+    locatedPositionUUID = [[NSUUID UUID] UUIDString];
+    
+    // Ask canvas to initialize
+    [self.canvas prepareCanvasWithSharedData:sharedData userDic:userDic andCredentialsUserDic:credentialsUserDic];
+    
+    // Table delegates; the delegate methods for attending these tables are part of this class.
+    self.tableRegister.delegate = self;
+    self.tableRegister.dataSource = self;
+    
+    [self.tableRegister reloadData];
+}
+
+/*!
+ @method didReceiveMemoryWarning
+ @discussion This method dispose of any resources that can be recreated id a memory warning is recived.
+ */
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Instance methods
+
+/*!
+ @method setCredentialsUserDic:
+ @discussion This method sets the NSMutableDictionary with the security purposes user credentials.
+ */
+- (void) setCredentialsUserDic:(NSMutableDictionary *)givenCredentialsUserDic
+{
+    credentialsUserDic = givenCredentialsUserDic;
+}
+
+/*!
+ @method setUserDic:
+ @discussion This method sets the NSMutableDictionary with the identifying purposes user credentials.
+ */
+- (void) setUserDic:(NSMutableDictionary *)givenUserDic
+{
+    userDic = givenUserDic;
+}
+
+/*!
+ @method setSharedData:
+ @discussion This method sets the shared data collection.
+ */
+- (void) setSharedData:(SharedData *)givenSharedData
+{
+    sharedData = givenSharedData;
+}
+
+/*!
+ @method setMotionManager:
+ @discussion This method sets the motion manager.
+ */
+- (void) setMotionManager:(MotionManager *)givenMotion
+{
+    motion = givenMotion;
+}
+
+/*!
+ @method setLocationManager:
+ @discussion This method sets the location manager.
+ */
+- (void) setLocationManager:(LocationManagerDelegate *)givenLocation
+{
+    location = givenLocation;
+}
+
+/*!
+ @method setItemBeaconIdNumber:
+ @discussion This method sets the NSMutableArray variable 'beaconsAndPositionsRegistered'.
+ */
+- (void) setItemBeaconIdNumber:(NSNumber *)givenItemBeaconIdNumber
+{
+    itemBeaconIdNumber = givenItemBeaconIdNumber;
+}
+
+/*!
+ @method setItemPositionIdNumber:
+ @discussion This method sets the NSMutableArray variable 'beaconsAndPositionsRegistered'.
+ */
+- (void) setItemPositionIdNumber:(NSNumber *)givenItemPositionIdNumber
+{
+    itemPositionIdNumber = givenItemPositionIdNumber;
+}
+
+#pragma mark - Buttons event handles
+
+/*!
+ @method handleButtonFinish:
+ @discussion This method handles the action in which the 'Finish' button is pressed.
+ */
+- (IBAction)handleButtonFinish:(id)sender
+{
+    // First, validate the access to the data shared collection
+    if (
+        [sharedData validateCredentialsUserDic:credentialsUserDic]
+        )
+    {
+        
+    } else {
+        
+    }
+    return;
+}
+
+/*!
+ @method handleButtonBack:
+ @discussion This method dismiss this view and ask main menu view to be displayed; 'prepareForSegue:sender:' method is called before.
+ */
+- (IBAction)handleButtonBack:(id)sender
+{
+    [self performSegueWithIdentifier:@"fromMONITORINGToSelectPosition" sender:sender];
+}
+
+/*!
+ @method alertUserWithTitle:andMessage:
+ @discussion This method alerts the user with a pop up window with a single "Ok" button given its message and title and lambda funcion handler.
+ */
+- (void) alertUserWithTitle:(NSString *)title
+                    message:(NSString *)message
+                 andHandler:(void (^)(UIAlertAction *action))handler;
+{
+    UIAlertController * alertUsersNotFound = [UIAlertController
+                                              alertControllerWithTitle:title
+                                              message:message
+                                              preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction * okButton = [UIAlertAction
+                                actionWithTitle:@"Ok"
+                                style:UIAlertActionStyleDefault
+                                handler:handler
+                                ];
+    
+    [alertUsersNotFound addAction:okButton];
+    [self presentViewController:alertUsersNotFound animated:YES completion:nil];
+    return;
+}
+
+/*!
+ @method prepareForSegue:sender:
+ @discussion This method is called before any segue and it is used for pass other views variables.
+ */
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSLog(@"[INFO][VCTTL] Asked segue %@", [segue identifier]);
+    
+    // If main menu is going to be displayed, any variable can be returned here
+    if ([[segue identifier] isEqualToString:@"fromMONITORINGToSelectPosition"]) {
+        
+        // Get destination view
+        ViewControllerSelectPositions * viewControllerSelectPositions = [segue destinationViewController];
+        // Set the variables
+        [viewControllerSelectPositions setCredentialsUserDic:credentialsUserDic];
+        [viewControllerSelectPositions setUserDic:userDic];
+        [viewControllerSelectPositions setSharedData:sharedData];
+        [viewControllerSelectPositions setMotionManager:motion];
+        [viewControllerSelectPositions setLocationManager:location];
+        
+        // Ask Location manager to clean the measures taken and reset its position.
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"stopLocationMeasuring"
+                                                            object:nil];
+        NSLog(@"[NOTI][VCTTL] Notification \"stopLocationMeasuring\" posted.");
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"resetLocationAndMeasures"
+                                                            object:nil];
+        NSLog(@"[NOTI][VCTTL] Notification \"resetLocationAndMeasures\" posted.");
+        return;
+    }
+    
+    if ([[segue identifier] isEqualToString:@"fromMONITORINGToModelingMONITORING"]) {
+        
+        // Get destination view
+        //ViewControllerModelingThetaThetaLocating * viewControllerModelingThetaThetaLocating = [segue destinationViewController];
+        // Set the variables
+        //[viewControllerModelingThetaThetaLocating setCredentialsUserDic:credentialsUserDic];
+        //[viewControllerModelingThetaThetaLocating setUserDic:userDic];
+        //[viewControllerModelingThetaThetaLocating setSharedData:sharedData];
+        //[viewControllerModelingThetaThetaLocating setMotionManager:motion];
+        //[viewControllerModelingThetaThetaLocating setLocationManager:location];
+        return;
+    }
+}
+
+#pragma mark - UItableView delegate methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (tableView == self.tableRegister) {
+        
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // Common to all cells
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure individual cells
+    if (tableView == self.tableRegister) {
+        
+        // Database could not be accessed.
+        if (
+            [sharedData validateCredentialsUserDic:credentialsUserDic]
+            )
+        {
+            
+        } else { // Database not acessible
+            [self alertUserWithTitle:@"Items won't be loaded."
+                             message:[NSString stringWithFormat:@"Database could not be accessed; please, try again later."]
+                          andHandler:^(UIAlertAction * action) {
+                              // TO DO: handle intrusion situations. Alberto J. 2019/09/10.
+                          }
+             ];
+            NSLog(@"[ERROR][VCTTL] Shared data could not be accessed while loading cells' item.");
+        }
+    }
+    
+    return cell;
+}
+@end
