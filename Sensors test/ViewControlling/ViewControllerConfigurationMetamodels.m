fb3dd7aed1638de40b08bebb0fb5d5aa03e49cc7
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2020-01-27 14:02:13 +0100

    Types and metamodels tables in configuration

--- Sensors test/ViewControlling/ViewControllerConfigurationMetamodels.m
+++ Sensors test/ViewControlling/ViewControllerConfigurationMetamodels.m
@@ -29,27 +29,30 @@
                                                    alpha:0.5
                                     ];
     
-    // Load existing metamodels in the device
+    // Load existing types and metamodels in the device
+    @"es.uam.miso/data/metamodels/areTypes"
+    @"es.uam.miso/data/metamodels/areMetamodels"
+    @"es.uam.miso/data/metamodels/types"
+    @"es.uam.miso/data/metamodels/metamodels"
     NSUserDefaults * userDefaults = [NSUserDefaults standardUserDefaults];
-    NSData * areMetamodelData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/areMetamodels"];
-    NSString * areMetamodel;
-    if (areMetamodelData) {
-        areMetamodel = [NSKeyedUnarchiver unarchiveObjectWithData:areMetamodelData];
+    
+    // Search for 'areTypes' boolean and if so, load the MDType array
+    NSData * areTypesData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/areTypes"];
+    NSString * areTypes;
+    if (areTypesData) {
+        areTypes = [NSKeyedUnarchiver unarchiveObjectWithData:areTypesData];
     }
-    
-    
-    if (areMetamodelData && areMetamodel && [areMetamodel isEqualToString:@"YES"]) {
-        // TO DO: Save the types using its name and no in an array. Alberto J. 15/11/2019.
-        // TO DO: Save several metamodels and no only one. Alberto J. 15/11/2019.
-        // Existing saved data
+    if (areTypesData && areTypes && [areTypes isEqualToString:@"YES"]) {
+        // Existing saved types
         
-        // Retrieve the metamodel array
-        NSData * metamodelData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/metamodel"];
-        NSMutableArray * types = [NSKeyedUnarchiver unarchiveObjectWithData:metamodelData];
+        // Retrieve the types MDType array
+        NSData * typesData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/types"];
+        types = [NSKeyedUnarchiver unarchiveObjectWithData:typesData];
         
-        NSLog(@"[INFO][VCCM] %tu metamodel types found in device.", types.count);
+        NSLog(@"[INFO][VCCM] %tu ontologycal types found in device.", types.count);
     } else {
         // No saved data
+        // TO DO: Ask user if demo types have to be loaded. Alberto J. 2020/01/27.
         
         // Create the types
         MDType * noType = [[MDType alloc] initWithName:@"<No type>"];
@@ -58,20 +61,68 @@
         MDType * wallType = [[MDType alloc] initWithName:@"Wall"];
         
         // Save them in persistent memory
-        areMetamodelData = nil; // ARC disposing
-        areMetamodelData = [NSKeyedArchiver archivedDataWithRootObject:@"YES"];
-        [userDefaults setObject:areMetamodelData forKey:@"es.uam.miso/data/metamodels/areMetamodels"];
+        areTypesData = nil; // ARC disposing
+        areTypesData = [NSKeyedArchiver archivedDataWithRootObject:@"YES"];
+        [userDefaults setObject:areTypesData forKey:@"es.uam.miso/data/metamodels/areTypes"];
         
-        NSMutableArray * types = [[NSMutableArray alloc] init];
+        types = [[NSMutableArray alloc] init];
         [types addObject:noType];
         [types addObject:cornerType];
         [types addObject:deviceType];
         [types addObject:wallType];
-        NSData * metamodelData = [NSKeyedArchiver archivedDataWithRootObject:types];
-        [userDefaults setObject:metamodelData forKey:@"es.uam.miso/data/metamodels/metamodel"];
+        NSData * typesData = [NSKeyedArchiver archivedDataWithRootObject:types];
+        [userDefaults setObject:typesData forKey:@"es.uam.miso/data/metamodels/types"];
         
-        NSLog(@"[INFO][VCCM] No metamodel found in device; demo metamodel saved.");
+        NSLog(@"[INFO][VCCM] No ontologycal types found in device; demo types saved.");
     }
+    
+    // Search for 'areMetamodels' boolean and if so, load the MDMetamodel array
+    NSData * areMetamodelsData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/areMetamodels"];
+    NSString * areMetamodels;
+    if (areMetamodelsData) {
+        areMetamodels = [NSKeyedUnarchiver unarchiveObjectWithData:areMetamodelsData];
+    }
+    if (areMetamodelData && areMetamodel && [areMetamodel isEqualToString:@"YES"]) {
+        // Existing saved metamodsels
+        
+        // Retrieve the metamodels array
+        NSData * metamodelData = [userDefaults objectForKey:@"es.uam.miso/data/metamodels/metamodels"];
+        metamodels = [NSKeyedUnarchiver unarchiveObjectWithData:metamodelData];
+        
+        NSLog(@"[INFO][VCCM] %tu metamodels found in device.", metamodels.count);
+    } else {
+        // No saved data
+        // TO DO: Ask user if demo metamodels have to be loaded. Alberto J. 2020/01/27.
+        
+        // Create the metamodel with a copy of types
+        NSMutableArray * metamodelTypes = [[NSMutableArray alloc] init];
+        for (MDType * type in types) {
+            [metamodelTypes addObject:type];
+        }
+        MDMetamodel * buildingMetamodel = [[MDMetamodel alloc] initWithName:@"Building"
+                                                                description:@"Building"
+                                                                   andTypes:metamodelTypes];
+        
+        // Save them in persistent memory
+        areMetamodelData = nil; // ARC disposing
+        areMetamodelData = [NSKeyedArchiver archivedDataWithRootObject:@"YES"];
+        [userDefaults setObject:areMetamodelData forKey:@"es.uam.miso/data/metamodels/areMetamodels"];
+        metamodels = [[NSMutableArray alloc] init];
+        [metamodels addObject:buildingMetamodel];
+        NSData * metamodelData = [NSKeyedArchiver archivedDataWithRootObject:metamodels];
+        [userDefaults setObject:metamodelData forKey:@"es.uam.miso/data/metamodels/metamodels"];
+        
+        NSLog(@"[INFO][VCCM] No metamodels found in device; demo metamodel saved.");
+    }
+
+    // Table delegates; the delegate methods for attending these tables are part of this class.
+    self.tableMetamodels.delegate = self;
+    self.tableMetamodels.dataSource = self;
+    self.tableTypes.delegate = self;
+    self.tableTypes.dataSource = self;
+    
+    [self.tableMetamodels reloadData];
+    [self.tableTypes reloadData];
     
 }
 
@@ -122,7 +173,15 @@
  */
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    
+    if (tableView == self.tableMetamodels) {
+	// A section for each metamodel
+        return metamodels.count;
+    }
+    if (tableView == self.tableTypes) {
+	// No sections for types
+        return 0;
+    }
+    return 0;
 }
 
 /*!
@@ -131,7 +190,35 @@
  */
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    
+    if (tableView == self.tableMetamodels) {
+	// Get the types in each metamodel and count its types
+        MDMetamodel * eachMetamodel = [metamodels objectAtIndex:section];
+	NSMutableArray * eachTypes = [eachMetamodel getTypes];
+        return eachTypes.count;
+    }
+    if (tableView == self.tableTypes) {
+        // Show a cell for each type
+        return types.count;
+    }
+    return 0;
+}
+
+/*!
+ @method tableView:titleForHeaderInSection:
+ @discussion Handles the upload of tables; returns each section title.
+ */
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if (tableView == self.tableMetamodels) {
+	// Get the name of each metamodel
+        MDMetamodel * eachMetamodel = [metamodels objectAtIndex:section];
+        return [eachMetamodel getName];
+    }
+    if (tableView == self.tableTypes) {
+	// No sections for types
+        return nil;
+    }
+    return nil;
 }
 
 /*!
@@ -140,7 +227,34 @@
  */
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
+    // Common to all cells
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure individual cells
+    if (tableView == self.tableMetamodels) {
+        
+        // Get the metamodel that shows this section...
+        MDMetamodel * eachMetamodel = [metamodels objectAtIndex:indexPath.section];
+        
+        // ...and get each of its types.
+        MDType * eachType = [[eachType getTypes] objectAtIndex:indexPath.row];
+        NSString * eachTypeName = [eachType getName];
+        cell.textLabel.text = [NSString stringWithFormat:@"%<@>", eachTypeName];
+        cell.textLabel.textColor = [UIColor colorWithWhite: 0.0 alpha:1];
+        
+    }
+    if (tableView == self.Types) {
+        MDType * eachType = [types objectAtIndex:indexPath.row];
+        NSString * eachTypeName = [eachType getName];
+        cell.textLabel.text = [NSString stringWithFormat:@"%<@>", eachTypeName];
+        cell.textLabel.textColor = [UIColor colorWithWhite: 0.0 alpha:1];
+    }
+    return cell;
 }
 
 /*!
